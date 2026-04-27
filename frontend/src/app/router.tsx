14518e5274b073a280bfb7e2a8eaa2bb3ff7d577
IsaacOrtgaAcosta <iortga.acosta@gmail.com>
2026-04-27 23:09:04 +0200

    chore: initialize monorepo structure

--- frontend/src/app/router.tsx
+++
@@ -1,7 +0,0 @@
-import { createBrowserRouter } from "react-router-dom";
-import { LoginPage } from "./features/auth/pages/login/LoginPage";
-
-
-export const router = createBrowserRouter([
-    { path: "/", element: <LoginPage />},
-])
